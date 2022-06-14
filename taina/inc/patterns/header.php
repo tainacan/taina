39aa91ef6855a9c9b5a48faca8502a416637699e
mateuswetah <mateus.m.luna@gmail.com>
2022-06-14 17:30:37 -0300

    Some alignments fixes for the header pattern.

--- taina/inc/patterns/header.php
+++ taina/inc/patterns/header.php
@@ -8,16 +8,16 @@ return array(
     'blockTypes' => array( 'core/template-part/header' ),
     'viewportWidth' => 1400,
     'inserter' => false,
-    'content'       => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-                        <div class="wp-block-group" style="margin-top: 0px;margin-bottom: 0px;padding-top: 0px;padding-right: 0px;padding-bottom: 0px;padding-left: 0px;">
+    'content'       => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":"0px"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"className":"is-style-taina-group-align-items_stretch"} -->
+                        <div class="wp-block-group is-style-taina-group-align-items_stretch" style="margin-top: 0px;margin-bottom: 0px;padding-top: 0px;padding-right: 0px;padding-bottom: 0px;padding-left: 0px;">
                             <!-- wp:site-title {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} /-->
                             <!-- wp:navigation {"ref":172974,"overlayBackgroundColor":"background-alt","layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap"},"style":{"typography":{"textTransform":"uppercase","lineHeight":"1.6"},"spacing":{"blockGap":"1.125em"}}} /-->
                         </div>
                         <!-- /wp:group -->
                             
-                        <!-- wp:group {"style":{"spacing":{"blockGap":"0px","margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"},"className":"is-style-taina-group-align-items_stretch"} -->
+                        <!-- wp:group {"style":{"spacing":{"blockGap":"0px","margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"className":"is-style-taina-group-align-items_stretch"} -->
                         <div class="wp-block-group is-style-taina-group-align-items_stretch" style="margin-top: 0px; margin-bottom: 0px">
-                            <!-- wp:site-tagline {"style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} /-->
+                            <!-- wp:site-tagline {"style":{"spacing":{"margin":{"top":"0px","right":"3,125em","bottom":"0px","left":"0px"}}}} /-->
                             <!-- wp:search {"placeholder": "'. esc_html('Search for...' , 'taina') . '", "label":"'. esc_html('Search' , 'taina') . '","showLabel":false,"buttonText":"'. esc_html('Search' , 'taina') . '","buttonUseIcon":true,"borderColor":"background","backgroundColor":"background-alt","textColor":"primary","className":"is-style-taina-collapsable-search"} /-->
                         </div>
                         <!-- /wp:group -->'
